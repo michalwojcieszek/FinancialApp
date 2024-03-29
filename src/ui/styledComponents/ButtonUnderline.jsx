62fa52cb9e53f76ad7260f2e0a5ab4aa9a0bed3b
michalwojcieszek <mwoj772@gmail.com>
2024-03-29 15:42:55 +0100

    login register link hover

--- src/ui/styledComponents/ButtonUnderline.jsx
+++ src/ui/styledComponents/ButtonUnderline.jsx
@@ -6,13 +6,25 @@ const ButtonUnderline = styled.button`
   cursor: pointer;
   font-size: 1.6rem;
   transition: background 1s;
-  border-bottom: 1px solid var(--color-blue-500);
+  /* border-bottom: 1px solid var(--color-blue-500); */
+  text-decoration: underline;
+  text-underline-offset: 0.4rem;
   margin-bottom: 3rem;
 
   &:hover {
+    text-decoration: none;
+  }
+
+  &:active,
+  &:focus {
+    text-underline-offset: 0.4rem;
+    text-decoration: underline;
+  }
+
+  /* &:hover {
     border-bottom: none;
     margin-bottom: 3.1rem;
-  }
+  } */
 `;
 
 export default ButtonUnderline;
